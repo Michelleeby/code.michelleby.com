b4ce5cdbdfd252b94235b15a244d570309964a1e
Michelleeby <michelle.elizabeth.byrnes@gmail.com>
2021-05-16 09:27:38 -0400

    Clean production files

--- _code_files/subsets_native-functions.rb
+++
@@ -1,8 +0,0 @@
-def subsets(set)
-  # Initialize accumulator with empty set.
-  acc = [ [] ]
-  # Push remaining combinations into accumulator
-  set.each_index{ |i| set.combination(i + 1){ 
-    |combination| acc.push(combination) } }
-  return acc
-end
